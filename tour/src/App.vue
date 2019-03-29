2ebdd54c1485ac1a99b134061729ca31cf4240a8
GaoFuq <1164842895@qq.com>
2019-03-29 13:26:01 +0800

    mm

---
+++ tour/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Home from './page/home/Home'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
