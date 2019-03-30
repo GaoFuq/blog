a726c0213f88a4a5912fe8aac3c91c88de500e73
GaoFuq <1164842895@qq.com>
2019-03-30 20:15:47 +0800

    home-catalog

--- tour/src/page/home/Home.vue
+++ tour/src/page/home/Home.vue
@@ -1,14 +1,22 @@
 <template>
- <div><home-header></home-header></div>
+ <div>
+ 	<home-header></home-header>
+	<home-swiper></home-swiper>
+	<home-catalog></home-catalog>
+ </div>
    
 </template>
 
 <script>
 import HomeHeader from './components/HomeHeader'
+import HomeSwiper from './components/HomeSwiper'
+import HomeCatalog from './components/HomeCatalog'
 export default {
   name: 'Home',
   components:{
-  	HomeHeader
+  	HomeHeader,
+  	HomeSwiper,
+  	HomeCatalog
   }
   
 }
---
+++ tour/src/page/home/components/HomeSwiper.vue
@@ -0,0 +1,60 @@
+<template>
+ <div class="swiper">
+    <swiper :options="swiperOption">
+    <!-- slides 轮播图 -->
+    <!-- <swiper-slide>
+      <img  class="swiper-img" src="https://pic5.40017.cn/03/000/71/c1/rB5oQFyCNUCAMP_sAAD2IbUPKZc944_520x218_00.jpg" alt="ss">
+    </swiper-slide>
+    <swiper-slide>
+      <img  class="swiper-img" src="https://pic5.40017.cn/03/000/e6/fa/rBANB1ybSOSAH-ffAAF-qQMYCa8154_520x218_00.jpg" alt="dd">
+    </swiper-slide>
+    <swiper-slide>
+      <img  class="swiper-img" src="https://pic5.40017.cn/03/000/28/73/rB5oQFxG7M2AOgxwAADChAKgVcA559_520x218_00.jpg" alt="xx">
+    </swiper-slide> -->
+    <swiper-slide v-for="item of headerImg" :key="item.id">
+      <img  class="swiper-img" :src="item.imgUrl" alt="dd">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div> 
+  </swiper>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data(){
+    return {
+      swiperOption:{
+        loop:true,
+        pagination:'.swiper-pagination'
+      },
+      headerImg:[
+        {
+          id:'001',
+          imgUrl:'https://pic5.40017.cn/03/000/71/c1/rB5oQFyCNUCAMP_sAAD2IbUPKZc944_520x218_00.jpg'
+        },
+        {
+          id:'002',
+          imgUrl:'https://pic5.40017.cn/03/000/e6/fa/rBANB1ybSOSAH-ffAAF-qQMYCa8154_520x218_00.jpg'
+        },
+        {
+          id:'003',
+          imgUrl: 'https://pic5.40017.cn/03/000/28/73/rB5oQFxG7M2AOgxwAADChAKgVcA559_520x218_00.jpg'
+        },
+      ]
+    }
+  }
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+.swiper-img
+    width:100%
+
+  
+  
+
+
+</style>
